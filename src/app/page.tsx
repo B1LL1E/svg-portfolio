cfed497b05a2b8939b85a9d4d07dd6e26bb8d92b
B1LL1E <karol660rata@gmail.com>
2025-12-14 00:49:42 +0100

    kafelki zostyl dodane do strony glownej

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ export default function Home() {
     // <main> to standardowy kontener dla głównej treści
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       {/** projekty */}
-      <div className="grid grid-cols-1 md:grid-cols-3">
+      <h1>Wybrane projekty</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-6xl mx-auto ">
         {projects.map((project) => (
-          <div key={project.id} title={project.title}>
-            <h1>{project.title}</h1>
+          <div key={project.id} // usunełem ze wzgledu na wyskakujacy zolty napis title={project.title}
+          className="group relative aspect-video bg-neutral-900 border border-neutral-800 flex items-center justify-center cursor-pointer overflow-hidden transition-all hover:border-white/50">
+            <h2 className="opacity-80 group-hover:opacity-100">{project.title}</h2>
           </div>
         ))}
       </div>
